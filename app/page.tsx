6e8819278b99cdda711ffb4a0243d8883f935fef
Sadat Shahriar <sadat@gakkmediabd.com>
2024-01-08 14:06:43 +0600

    initial project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <h2 className={`mb-3 text-2xl font-semibold`}>
+        Revolution Education Consultency
+      </h2>
+    </main>
+  );
+}
